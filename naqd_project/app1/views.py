03052bd6972335d258fb659eb255cb74163f92a0
“EngHusamSaad” <eng.husam.saed@gmail.com>
2025-01-07 01:40:29 +0200

    Fix add payments Bugs

--- naqd_project/app1/views.py
+++ naqd_project/app1/views.py
@@ -180,7 +180,7 @@ def customers_view(request):
 def debt_view(request):
     customer_id = request.GET.get('customer_id')
     try:
-        debts = Debt.objects.filter(customer_id=customer_id)
+        debts = Debt.objects.filter(customer_id=customer_id,status_debt=False)
         
         debt_data = [
         {
@@ -270,24 +270,22 @@ def add_payment(request):
         try:
             customer = Customer.objects.get(id=customer_id)
             debt = Debt.objects.get(id=debt_id)
-            print(type(debt.amount_debt))
-            print(type(float(payment_amount)))
-            
-            print((debt.amount_debt))
-            print((float(payment_amount)))
-            
-            # print(debt.amount_debt)
-            if debt.amount_debt>=float(payment_amount):
+           
+            if debt.amount_debt==float(payment_amount):
                 payment = Paymnet.objects.create(
                     debt=debt,
                     amount_payment=payment_amount,
                     payment_type=payment_type,
                     
                 )
+                
+                debt.status_debt=True
+                debt.save()
+                
                 response = {'success': True}
             else:
                 print("in if create")
-                response = {'success': False, 'error': 'Payemnt Amount is large of Debt Amount !!!'}   
+                response = {'success': False, 'error': 'Payemnt Amount should Equal Debt Amount !!!'}   
             
         except Customer.DoesNotExist:
             response = {'success': False, 'error': 'Customer not found'}
